0cf41392668e09ce7c07d6fa8ab8eb067e2159ac
Gil Ortega <gildardo.ortega181945@potros.itson.edu.mx>
2024-12-13 11:47:47 -0700

    Segundo commit

--- src/main/java/com/example/inforcomtech/controller/HomeController.java
+++ src/main/java/com/example/inforcomtech/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.inforcomtech.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
+	
+	//creaos un modelo para podrer mostrarlo en home.html
 	@GetMapping("/")
-	public String mostrarHome() {
+	public String mostrarHome(Model model) {
+		model.addAttribute("mensaje", "Hola Bienvenidos Puntual CCare App");
 		
 		
 		return "home";
